7bdffcc7d9b3a2b2975973275763f27083a5b8c1
moiz303 <moiz.izraylit@yandex.ru>
2024-01-11 15:02:51 +0300

    Unresolved Files

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
-import time
-from threading import Thread
+import pygame
+import os
+import sys
 
 
-# Пишем функции для каждого потока
-def thread1_func():
-    while True:
-        time.sleep(2)
-        print("Hello")
+all_sprites = pygame.sprite.Group()
+cou = 0
 
 
-def thread2_func():
-    while True:
-        time.sleep(4)
-        print("Привет")
+def load_image(name):
+    fullname = name
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
-def thread3_func():
-    while True:
-        time.sleep(6)
-        print("Здравствуйте")
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, 
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
 
 
-t1 = Thread(target=thread1_func)  # В именованный аргумент `target` передаём функцию,
-t1.start()                        # которая будет выполнятся в этом потоке.
-t2 = Thread(target=thread2_func)
-t2.start()
-t3 = Thread(target=thread3_func)
-t3.start()
+screen = pygame.display.set_mode((200, 200))
+clock = pygame.time.Clock()
+dragon = AnimatedSprite(load_image("dragon_sheet8x2.xcf"), 8, 2, 50, 50)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    dragon.update()
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(10)
+pygame.quit()
